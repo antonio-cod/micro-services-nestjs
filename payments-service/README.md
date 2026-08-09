ec9de47e3f8438e2cff55e2d2cab4d40c8f3d5bf
Antonio Carlos <antonio_carlostads@hotmail.com>
2026-08-09 15:53:26 -0300

    Feat: Estrutura inicial para consumir mensagens no Payment Service

--- payments-service/README.md
+++ payments-service/README.md
@@ -2,3 +2,8 @@
 
     npm install @nestjs/typeorm typeorm pg @nestjs/passport passport passport-jwt @nestjs/jwt bcryptjs @nestjs/axios axios @nestjs/config class-validator class-transformer amqplib
     npm install -D @types/passport-jwt @types/bcryptjs @types/amqplib @types/passport
+
+# Nestjs cli
+
+    nest generate module events
+    nest generate service events/rabbitmq --no-spec
---
+++ payments-service/src/events/events.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { RabbitmqService } from './rabbitmq/rabbitmq.service';
+import { ConfigModule } from '@nestjs/config';
+import { PaymentQueueService } from './payment-queue/payment-queue.service';
+import { PaymentQueueService } from './payment-queue/payment-queue.service';
+
+@Module({
+  imports: [ConfigModule],
+  providers: [RabbitmqService, PaymentQueueService],
+})
+export class EventsModule {}
