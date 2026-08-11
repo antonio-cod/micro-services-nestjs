7bd5066ada18df18812d1ed87ecd92387e5f9c22
Antonio Carlos <antonio_carlostads@hotmail.com>
2026-08-11 11:56:59 -0300

    Feat: Implementando o servico de reprocessamento da DQL

--- payments-service/src/events/events.module.ts
+++ payments-service/src/events/events.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
-import { RabbitmqService } from './rabbitmq/rabbitmq.service';
 import { ConfigModule } from '@nestjs/config';
-import { PaymentQueueService } from './payment-queue/payment-queue.service';
 import { PaymentConsumerService } from './payment-consumer/payment-consumer.service';
+import { PaymentQueueService } from './payment-queue/payment-queue.service';
+import { RabbitmqService } from './rabbitmq/rabbitmq.service';
+import { DlqService } from './dlq/dlq.service';
 
 @Module({
   imports: [ConfigModule],
-  providers: [RabbitmqService, PaymentQueueService, PaymentConsumerService],
+  providers: [
+    RabbitmqService,
+    PaymentQueueService,
+    PaymentConsumerService,
+    DlqService,
+  ],
 })
 export class EventsModule {}
